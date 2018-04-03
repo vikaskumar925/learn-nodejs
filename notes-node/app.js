b6b41d7dff0afc3662a5b4edb2bada66c77022ea
Vikas Kumar <negi.vikash925@gmail.com>
2018-04-03 21:50:42 +0530

    Using Require In Loading node modules and own files

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,8 +1,10 @@
-console.log('Starting App');
+console.log('Starting App.js');
 
 const fs = require('fs');
 const os = require('os');
 
+const notes= require('./notes.js')
 var user = os.userInfo();
-
-fs.appendFileSync("greetin.txt",`Hello ${user.username} !` );
+var res = notes.add(3, 4);
+console.log("Result: ",res);
+fs.appendFileSync("greetin.txt",`Hello ${user.username} ! You are ${notes.age}.` );
---
+++ notes-node/notes.js
@@ -0,0 +1,7 @@
+console.log('Starting Notes.js');
+
+module.exports.addNote = () => {
+    console.log("add Notes");
+    return "New Note";
+};
+module.exports.add = (a, b) => a + b;
