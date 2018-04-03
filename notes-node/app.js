29e956c7cc141d62eb2ccf5f6037752e1a232a5a
Vikas Kumar <negi.vikash925@gmail.com>
2018-04-03 22:24:58 +0530

    Installing and using 3rd party node modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,9 +2,17 @@ console.log('Starting App.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 
 const notes= require('./notes.js')
 var user = os.userInfo();
 var res = notes.add(3, 4);
-console.log("Result: ",res);
+
+
+
+// console.log(_.isString(true));
+// console.log(_.isString('test'));
+
+var filteredArray = _.uniq(['Vikas', 1, 'Vikas', 1, 2]);
+console.log(filteredArray);
 fs.appendFileSync("greetin.txt",`Hello ${user.username} ! You are ${notes.age}.` );
