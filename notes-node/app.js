52c3e2bd7755db08f1afeba90f126a9298388152
Vikas Kumar <negi.vikash925@gmail.com>
2018-04-05 21:19:27 +0530

    Getting input from users

--- notes-node/app.js
+++ notes-node/app.js
@@ -13,6 +13,20 @@ var res = notes.add(3, 4);
 // console.log(_.isString(true));
 // console.log(_.isString('test'));
 
-var filteredArray = _.uniq(['Vikas', 1, 'Vikas', 1, 2]);
-console.log(filteredArray);
-fs.appendFileSync("greetin.txt",`Hello ${user.username} ! You are ${notes.age}.` );
+// var filteredArray = _.uniq(['Vikas',1, 2, 'Vikas', 5,2]);
+// console.log(filteredArray);
+// fs.appendFileSync("greetin.txt",`Hello ${user.username} ! You are ${notes.age}.` );
+var command = process.argv[2];
+console.log(process.argv);
+console.log("Command: ", command);
+if(command ==='add'){
+    console.log("adding new note");
+} else if (command === "list"){
+    console.log("listing all notes");
+} else if (command==="read"){
+    console.log("Fetch the note");
+}else if (command==="remove"){
+    console.log("Remove the note");
+}else {
+    console.log("Command not recognized");
+}
