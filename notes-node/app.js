0c5e36831ea98e6dd3d4f631d96d6e882e79493e
Vikas Kumar <negi.vikash925@gmail.com>
2018-04-09 21:04:28 +0530

    Simplified Inputs with Yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,27 +6,23 @@ const _ = require('lodash');
 
 const notes= require('./notes.js')
 var user = os.userInfo();
-var res = notes.add(3, 4);
 
+const yargs = require('yargs');
 
-
-// console.log(_.isString(true));
-// console.log(_.isString('test'));
-
-// var filteredArray = _.uniq(['Vikas',1, 2, 'Vikas', 5,2]);
-// console.log(filteredArray);
-// fs.appendFileSync("greetin.txt",`Hello ${user.username} ! You are ${notes.age}.` );
+const argv = yargs.argv;
 var command = process.argv[2];
-console.log(process.argv);
+
 console.log("Command: ", command);
+
+console.log("yargs: ", argv);
 if(command ==='add'){
-    console.log("adding new note");
+    notes.addNote(argv.title,argv.body);
 } else if (command === "list"){
-    console.log("listing all notes");
+    notes.getAll();
 } else if (command==="read"){
-    console.log("Fetch the note");
+    notes.getNote(argv.title);
 }else if (command==="remove"){
-    console.log("Remove the note");
+    notes.removeNote(argv.title);
 }else {
     console.log("Command not recognized");
 }
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,20 @@
 console.log('Starting Notes.js');
 
-module.exports.addNote = () => {
-    console.log("add Notes");
-    return "New Note";
+var addNote = (title, body) => {
+    console.log("Adding note ", title, body);
 };
-module.exports.add = (a, b) => a + b;
+var getAll = () => {
+    console.log("Getting all notes");
+};
+var getNote = (title) => {
+    console.log("reading Note of title ", title);
+};
+var removeNote = (title) => {
+    console.log("remove the note of title ", title);
+};
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote,
+};
