de2cf9eb638170d8f7287f4f2de5a271e00d6ccf
vikaskumar925 <negi.vikash925@gmail.com>
2018-04-11 20:21:19 +0530

    Print the formatted response object using JSON.stringify

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
 request({
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url:'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBoXbVZTU8ipGC3GNJPXs2TyOYBwZcKDI0&address=1301%20lombard%20street%20philadelphia',
     json:true,
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));//pretty printing objects
 });
