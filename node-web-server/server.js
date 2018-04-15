018cdb346ecccc913ffcfec7558347c665c3428e
vikaskumar925 <negi.vikash925@gmail.com>
2018-04-15 17:09:22 +0530

    Rendering Templates with Data

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,20 +2,22 @@ const express = require('express');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express</h1>');
-    res.send({
-        name:'Vikas',
-        likes:[
-            'biking',
-            'cities'
-        ],
+    res.render('home.hbs', {
+        pageTitle:'Home',
+        currentYear:new Date().getFullYear(),
+        welcomeMessage:'Hey Welcome to The home page'
     });
 });
 app.get('/about',(req, res) => {
-    res.send('About');
+    res.render('about.hbs', {
+        pageTitle:'About Page',
+        currentYear:new Date().getFullYear(),
+    });
 } );
 
 app.get('/bad', (req, res) => {
